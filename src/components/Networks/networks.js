b321a517a3229f762217c5188d75fab991f11bde
Pavel_Kazlou <pnkazlou@ibagroup.eu>
2024-05-30 10:32:05 +0300

    add api button when no data

--- src/components/Networks/networks.js
+++ src/components/Networks/networks.js
@@ -4,9 +4,11 @@ import NetworksList from './networksList';
 import NetworkModal from './networkModal';
 import { fetchNetworks, fetchVMs, fetchProvider } from '../../AppActions';
 import { withRouter } from 'react-router-dom';
-import { Loader } from 'semantic-ui-react';
+import { Grid, Header, Loader } from 'semantic-ui-react';
+import { networkValue } from '../../constants/common';
 
 const ContentPage = React.lazy(() => import('container/ContentPage'));
+const ApiButton = React.lazy(() => import('container/ApiButton'));
 
 const Networks = ({ t, history }) => {
     const networks = useSelector(state => state.VpcStore.networks);
@@ -15,6 +17,8 @@ const Networks = ({ t, history }) => {
     const vmsFetchStatus = useSelector(state => state.VpcStore.assignedVmsFetchStatus);
     const providerIdFetchStatus = useSelector(state => state.VpcStore.providerIdFetchStatus);
     const user = useSelector(state => state.host.user);
+    const baseUrls = useSelector(state => state.host.baseUrls);
+    const providerId = useSelector(state => state.VpcStore.providerId);
 
     window.goToRootRoute = () => history.push('/vpc');
 
@@ -28,12 +32,35 @@ const Networks = ({ t, history }) => {
         }
     }, [dispatch, user]);
 
+    const isNoData = networks.length < 1;
+
     return (
         <React.Suspense fallback={
             <Loader active inline='centered' />
         }>
-        <ContentPage t={t} statuses={[vmsFetchStatus, networksFetchStatus, providerIdFetchStatus]} pageData={networks.length ? [vms, networks] : []} title={'vpcNetworks'}
-            componentDataList={NetworksList} noContentMessage={'noNetworks'} noContentComponentModal={NetworkModal}
+        {isNoData && (
+            <Grid className='no-vpc-networks-header'>
+                <Grid.Row style={{padding: "0"}}>
+                    <Header as='h4' content={t('vpcNetworks')} />
+                    <div className='buttons-vpc'>
+                        <ApiButton element='network'
+                            item={networkValue}
+                            user={user}
+                            providerId={providerId}
+                            locationUrl={baseUrls[user.location]}
+                        />
+                        <NetworkModal t={t} />
+                    </div>
+                </Grid.Row>
+                <Grid.Row>
+                    <div style={{ maxWidth: '600px' }}>
+                        <p className='color--grey'>{t('vpcDescription')}</p>
+                    </div>
+                </Grid.Row>
+            </Grid>
+        )}
+        <ContentPage t={t} statuses={[vmsFetchStatus, networksFetchStatus, providerIdFetchStatus]} pageData={networks.length ? [vms, networks] : []} title={isNoData ? "" : 'vpcNetworks'}
+            componentDataList={NetworksList} noContentMessage={'noNetworks'} 
         />
         </React.Suspense>
     );
--- src/components/Routes/routes.js
+++ src/components/Routes/routes.js
@@ -3,16 +3,20 @@ import { useSelector, useDispatch } from 'react-redux';
 import RoutesList from './routesList';
 import { fetchRoutes, fetchProvider } from '../../AppActions';
 import { withRouter } from 'react-router-dom';
-import { Loader } from 'semantic-ui-react';
+import { Grid, Header, Loader } from 'semantic-ui-react';
 import RouteModal from './routeModal';
+import { routesValue } from '../../constants/common';
 
 const ContentPage = React.lazy(() => import('container/ContentPage'));
+const ApiButton = React.lazy(() => import('container/ApiButton'));
 
 const Routes = ({ t, history }) => {
     const routes = useSelector(state => state.VpcStore.routes);
     const routesFetchStatus = useSelector(state => state.VpcStore.routesFetchStatus);
     const providerIdFetchStatus = useSelector(state => state.VpcStore.providerIdFetchStatus);
     const user = useSelector(state => state.host.user);
+    const baseUrls = useSelector(state => state.host.baseUrls);
+    const routerId = useSelector(state => state.VpcStore.routerId);
 
     window.goToRootRoute = () => history.push('/vpc');
 
@@ -23,12 +27,30 @@ const Routes = ({ t, history }) => {
         providerIdFetchStatus !== 'fulfilled' && dispatch(fetchProvider());
     }, [dispatch, user]);
 
+    const isNoData = routes.length < 1;
+
     return (
         <React.Suspense fallback={
             <Loader active inline='centered' />
         }>
-        <ContentPage t={t} statuses={[routesFetchStatus, providerIdFetchStatus]} pageData={routes} title={'routes'}
-            componentDataList={RoutesList} noContentMessage={'noRoutes'} noContentComponentModal={RouteModal}
+        {isNoData && (
+            <Grid className='no-vpc-routes-header'>
+                <Grid.Row style={{padding: "0"}}>
+                    <Header as='h4' content={t('routes')} />
+                    <div className='buttons-vpc'>
+                        <ApiButton element='route'
+                            item={routesValue}
+                            user={user}
+                            providerId={routerId}
+                            locationUrl={baseUrls[user.location]}
+                        />
+                        <RouteModal t={t} />
+                    </div>
+                </Grid.Row>
+            </Grid>
+        )}
+        <ContentPage t={t} statuses={[routesFetchStatus, providerIdFetchStatus]} pageData={routes} title={isNoData ? '' : 'routes'}
+            componentDataList={RoutesList} noContentMessage={'noRoutes'}
         />
         </React.Suspense>
     );
