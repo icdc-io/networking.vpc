a252264632dc15ee282e4da273f0076d5e6ff6ce
DValashchuk@ibagroup.eu <DValashchuk@ibagroup.eu>
2024-08-01 14:52:47 +0300

    Fix UI blinding

--- src/components/overview.js
+++ src/components/overview.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
 import { Loader, Segment } from "semantic-ui-react";
 import TabsLayout from "../general/tabsLayout";
 import { networksPath, networkPath, routesPath } from "../constants/routes";
@@ -9,8 +9,14 @@ const NetworkDetails = React.lazy(() => import("./Details/networkDetails"));
 const Networks = React.lazy(() => import("./Networks/networks"));
 const RoutesPage = React.lazy(() => import("./Routes/routes"));
 
-const NetworksOverview = () => {
+const RootComponent = () => {
   const { t } = useTranslation();
+  const location = useLocation();
+  const isRootBalancerPath =
+    location.pathname.split("/").filter((route) => route).length < 3;
+
+  if (isRootBalancerPath) return <Navigate to={networksPath()} replace />;
+
   const menuItems = [
     {
       name: t("networks"),
@@ -28,19 +34,24 @@ const NetworksOverview = () => {
     <>
       <TabsLayout menuItems={menuItems} />
       <Segment attached="bottom">
-        <React.Suspense fallback={<Loader active inline="centered" />}>
-          <Routes>
-            <Route exact path={networksPath()} Component={Networks} />
-            <Route exact path={networkPath()} Component={NetworkDetails} />
-            <Route exact path={routesPath()} Component={RoutesPage} />
-            <Route
-              path="*"
-              element={<Navigate to={menuItems[0].path} replace />}
-            />
-          </Routes>
-        </React.Suspense>
+        <Outlet />
       </Segment>
     </>
+  );
+};
+
+const NetworksOverview = () => {
+  return (
+    <React.Suspense fallback={<Loader active inline="centered" />}>
+      <Routes>
+        <Route path="/" Component={RootComponent}>
+          <Route path={networksPath()} Component={Networks} />
+          <Route path={networkPath()} Component={NetworkDetails} />
+          <Route path={routesPath()} Component={RoutesPage} />
+        </Route>
+        <Route path="*" element={<Navigate to={networksPath()} replace />} />
+      </Routes>
+    </React.Suspense>
   );
 };
 
